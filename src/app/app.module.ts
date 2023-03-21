b23a2d253d9265a3e6620885bb9fd0b08f36cf63
KuyaShawn <47099829+KuyaShawn@users.noreply.github.com>
2023-03-20 22:56:35 -0700

    Added/created files for other pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,28 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { LoginComponent } from './login/login.component';
+import { LogoutComponent } from './logout/logout.component';
+import { ReadEmailComponent } from './read-email/read-email.component';
+import { SignupComponent } from './signup/signup.component';
+import { AboutComponent } from './about/about.component';
+import { AdminComponent } from './admin/admin.component';
+import { ContactComponent } from './contact/contact.component';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    LoginComponent,
+    LogoutComponent,
+    ReadEmailComponent,
+    SignupComponent,
+    AboutComponent,
+    AdminComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/read-email/read-email.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-read-email',
+  templateUrl: './read-email.component.html',
+  styleUrls: ['./read-email.component.css']
+})
+export class ReadEmailComponent {
+
+}
