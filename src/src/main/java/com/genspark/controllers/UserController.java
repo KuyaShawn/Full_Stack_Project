cce76cf13e1e63d719bdfa7b5624754db27fc301
KuyaShawn <47099829+KuyaShawn@users.noreply.github.com>
2023-03-26 13:54:12 -0700

    updated application properties and User Controller
    updated application properties and User Controller

--- src/src/main/java/com/genspark/controllers/UserController.java
+++ src/src/main/java/com/genspark/controllers/UserController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin(origins = "*") // Add this line to enable CORS for your Angular app
 @RestController
 public class UserController implements UserFactory {
   @Autowired
@@ -22,22 +23,22 @@ public class UserController implements UserFactory {
   }
   @GetMapping("/users/{id}")
   @Override
-  public UserEntity getUserById(int id) {
+  public UserEntity getUserById(@PathVariable("id")int id) {
     return userService.getUserById(id);
   }
   @PostMapping("/users")
   @Override
-  public UserEntity addUser(UserEntity user) {
+  public UserEntity addUser(@RequestBody UserEntity user) {
     return userService.addUser(user);
   }
   @PutMapping("/users/{user}")
   @Override
-  public UserEntity updateUser(UserEntity user) {
+  public UserEntity updateUser(@RequestBody UserEntity user) {
     return userService.updateUser(user);
   }
   @DeleteMapping("/users/{id}")
   @Override
-  public String deleteUserById(int id) {
+  public String deleteUserById(@PathVariable("id") int id) {
     return userService.deleteUserById(id);
   }
 }
